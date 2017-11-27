20ad162bc11a48a6a3d92d3a4c5cc7b68ea9c8f2
grantspell <spellgrant@gmail.com>
2017-11-27 15:03:12 -0500

    UPDATED | landing page styling

--- client/src/components/home/HomeComp.js
+++ client/src/components/home/HomeComp.js
@@ -13,8 +13,8 @@ const Wrapper = styled.div`
 `
 const Bubble = styled.div`
     background-color: pink;
-    height: 60vh;
-    width: 60vw;
+    height: 600px;
+    width: 600px;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -25,25 +25,47 @@ const Bubble = styled.div`
 const Title = styled.div`
     text-align: center;
     width: 100%;
+    margin-top: 75px;
 
     h1 {
         color: white;
         font-family: 'Barlow Semi Condensed', sans-serif;
         font-size: 50px;
         text-shadow: 3px 3px 0px black;
+        margin-bottom: -9px;
+        letter-spacing: 10px;
     }
     p {
         width: 100%;
         color: black;
-        letter-spacing: 5px;
+        letter-spacing: 9.5px;
         padding: 3px;
     }
 `
 const Buttons = styled.div`
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     width: 30%;
-    
+
+    .aButton {
+        background-color: rgba(255, 255, 255, .0);
+        border: none;
+        padding: 0px;
+        text-align: center;
+        text-decoration: none;
+    }
+    .material-icons {
+        color: white;
+        font-size: 48px;
+    }
+    p {
+        color: black;
+        letter-spacing: 5px;
+        background-color: white;
+
+    }
 `
 
 class HomeComp extends Component {
@@ -57,9 +79,7 @@ class HomeComp extends Component {
                     </Title>
                     <Buttons>
 
-                        <button><Link to="/account">Log In</Link></button>
-
-                        <button><Link to="/account">Sign Up</Link></button>
+                        <button className="aButton"><Link to="/account" alt="Account Login or Signup Button"><i class="material-icons">person_outline</i></Link></button>
                         
                     </Buttons>
                 </Bubble>
